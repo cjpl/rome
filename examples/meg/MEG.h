4cf3d9dcdd05edea88a60cef4951d45176c560d1
schneebeli <schneebeli@306b0a18-5003-0410-92f3-cc1c673a7936>
2004-06-15 16:55:23 +0000

    steering parameters, bank access
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/rome/trunk@19 306b0a18-5003-0410-92f3-cc1c673a7936

--- examples/meg/MEG.h
+++ examples/meg/MEG.h
@@ -1,7 +1,7 @@
 #ifndef MEG_H
 #define MEG_H
 
-// Definitions
+// Faces
 #define TOP     200
 #define BOTTOM  201
 #define FRONT   202
@@ -9,45 +9,5 @@
 #define RIGHT   204
 #define LEFT    205
 #define NOFACE  206
-
-#define INVALID -1000.f
-#define SUCCESS 1
-#define ERROR   0
-
-// Photo Multiplier
-#define gNumberOfPMT             257
-#define gNumberOfFTDC             64
-#define gNumberOfCTDC             80
-#define gNumberOfVTDC            128
-#define gNumberOfLXePMT          228
-#define gPedestalThreshold       500.
-#define gPedestalSpreadThreshold   1.
-#define gNumberOfMapHistos        10
-
-// Midas
-#define gMaxMidasBankSize 10000
-
-// midas bank structures
-typedef struct {
-   unsigned short int data     : 12;
-   unsigned short int q1       : 4;
-   unsigned short int channel  : 6;
-   unsigned short int q2       : 1;
-   unsigned short int range    : 1;
-   unsigned short int event    : 3;
-   unsigned short int geo_addr : 5;
-} LRS1875_DATA;
-
-typedef struct {
-   unsigned short int data     : 12;
-   unsigned short int overflow : 1;
-   unsigned short int underflow: 1;
-   unsigned short int valid    : 1;
-   unsigned short int q1       : 1;
-   unsigned short int channel  : 6;
-   unsigned short int q2       : 2;
-   unsigned short int tag      : 3;
-   unsigned short int geo_addr : 5;
-} CAEN775_DATA;
 
 #endif   // MEG_H
