06026128ec2965e660ee2ec20d1be847e7deba8e
sawada <sawada@306b0a18-5003-0410-92f3-cc1c673a7936>
2005-07-03 17:59:08 +0000

    start version 1.5
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/rome/trunk/argus@612 306b0a18-5003-0410-92f3-cc1c673a7936

--- trunk/include/ArgusVersion.h
+++ trunk/include/ArgusVersion.h
@@ -1,8 +1,8 @@
 #ifndef ARGUSVERSION_H
 #define ARGUSVERSION_H
 
-#define ARGUS_RELEASE "Version 1.04.00"
-#define ARGUS_VERSION_CODE 66560
+#define ARGUS_RELEASE "Version 1.05.00"
+#define ARGUS_VERSION_CODE 66816
 #define ARGUS_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
 
 #endif
