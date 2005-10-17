6013dac0b3274727d824a2bdb29c4451cd8b6753
ritt <ritt@306b0a18-5003-0410-92f3-cc1c673a7936>
2005-10-17 09:55:07 +0000

    Moved remotely
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/rome/trunk/rome@466 306b0a18-5003-0410-92f3-cc1c673a7936

---
+++ examples/meg/MEG.h
@@ -0,0 +1,13 @@
+#ifndef MEG_H
+#define MEG_H
+
+// Faces
+#define TOP     200
+#define BOTTOM  201
+#define FRONT   202
+#define BACK    203
+#define RIGHT   204
+#define LEFT    205
+#define NOFACE  206
+
+#endif   // MEG_H
