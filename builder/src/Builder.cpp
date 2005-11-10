70353f202c9a07a91c07fad12cc9cac30bb8b5a6
sawada <sawada@306b0a18-5003-0410-92f3-cc1c673a7936>
2005-11-10 16:27:49 +0000

    correct compare of numofFolder, numofTab.
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/rome/trunk/argus@714 306b0a18-5003-0410-92f3-cc1c673a7936

--- builder/src/Builder.cpp
+++ builder/src/Builder.cpp
@@ -732,15 +732,15 @@ void ArgusBuilder::WriteMakefile()
    buffer.AppendFormatted("\n");
 
 #if defined( R__UNIX )
-   if (numOfMFDictHeaders==0) {
+   if (numOfMFDictHeaders == 0) {
       buffer.AppendFormatted("ifdef DictionaryHeaders\n");
       buffer.AppendFormatted("objects += obj/%sUserDict.obj\n",shortCut.Data());
       buffer.AppendFormatted("endif\n");
    }
-   if (numOfFolder<=0) {
+   if (numOfFolder > 0) {
       buffer.AppendFormatted("objects += obj/%sFolderDict.obj\n",shortCut.Data());
    }
-   if (numOfTab<=0) {
+   if (numOfTab > 0) {
       buffer.AppendFormatted("objects += obj/%sTabDict.obj\n",shortCut.Data());
    }
 #else
