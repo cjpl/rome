4f5e97ca83c5502c0cca803145930d64bf6cd257
schneebeli_m <schneebeli_m@306b0a18-5003-0410-92f3-cc1c673a7936>
2005-11-22 06:50:00 +0000

    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/rome/trunk/rome@746 306b0a18-5003-0410-92f3-cc1c673a7936

---
+++ include/ROMEDataBaseDAQ.h
@@ -0,0 +1,32 @@
+/********************************************************************
+  ROMEDataBaseDAQ.h, M. Schneebeli PSI
+
+  $Id: ROMEDataBaseDAQ.h 480 2005-10-17 12:12:54Z schneebeli_m $
+
+********************************************************************/
+#ifndef ROMEDataBaseDAQ_H
+#define ROMEDataBaseDAQ_H
+
+#include <ROMEAnalyzer.h>
+#include <ROMEDAQSystem.h>
+
+class ROMEDataBaseDAQ : public ROMEDAQSystem {
+protected:
+
+   int           fTimeStamp;                       //! Current time stamp
+
+public:
+   ROMEDataBaseDAQ();
+   virtual ~ROMEDataBaseDAQ() {};
+
+   int   GetTimeStamp() { return fTimeStamp; };
+   const char* GetName() { return "rome"; };
+
+   bool Init();
+   bool BeginOfRun();
+   bool Event(int event);
+   bool EndOfRun();
+   bool Terminate();
+};
+
+#endif   // ROMEDataBaseDAQ_H
