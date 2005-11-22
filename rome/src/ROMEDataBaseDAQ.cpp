b596688532811848b57140b92715eac379ccc00d
schneebeli_m <schneebeli_m@306b0a18-5003-0410-92f3-cc1c673a7936>
2005-11-22 06:50:00 +0000

    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/rome/trunk@746 306b0a18-5003-0410-92f3-cc1c673a7936

---
+++ rome/src/ROMEDataBaseDAQ.cpp
@@ -0,0 +1,54 @@
+// Author: Matthias Schneebeli
+//////////////////////////////////////////////////////////////////////////
+//
+//  ROMEDataBaseDAQ
+//
+//  Interface to a database used as the main input.
+//
+//  $Id: ROMEDataBaseDAQ.cpp 680 2005-10-31 11:32:46Z ritt $
+//
+//////////////////////////////////////////////////////////////////////////
+
+#include <ROMEDataBaseDAQ.h>
+#include <ROMEAnalyzer.h>
+
+ROMEDataBaseDAQ::ROMEDataBaseDAQ() {
+}
+
+bool ROMEDataBaseDAQ::Init() {
+   if (gROME->isOnline()) {
+      gROME->Println("Database mode is not supported for online analysis.\n");
+      return false;
+   }
+   if (gROME->isOffline()) {
+   }
+   return true;
+}
+bool ROMEDataBaseDAQ::BeginOfRun() {
+   if (gROME->isOffline()) {
+      gROME->SetCurrentEventNumber(0);
+      this->SetAnalyze();
+      this->SetRunning();
+   }
+   return true;
+}
+bool ROMEDataBaseDAQ::Event(int event) {
+   if (gROME->isOffline()) {
+      gROME->SetCurrentEventNumber(event);
+   }
+
+   return true;
+}
+bool ROMEDataBaseDAQ::EndOfRun() {
+   if (gROME->isOffline()) {
+   }
+   return true;
+}
+bool ROMEDataBaseDAQ::Terminate() {
+   if (gROME->isOffline()) {
+   }
+   return true;
+}
+
+
+
