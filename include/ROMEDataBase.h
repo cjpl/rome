45719f12942e10529e482b7e32c0146ffc8f8f2b
sawada <sawada@306b0a18-5003-0410-92f3-cc1c673a7936>
2005-11-30 18:35:47 +0000

    default value of ROMEDataBase::Write and Read. Initialize data members before using.
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/rome/trunk/rome@760 306b0a18-5003-0410-92f3-cc1c673a7936

--- include/ROMEDataBase.h
+++ include/ROMEDataBase.h
@@ -17,8 +17,8 @@ public:
    ROMEDataBase() {};
    virtual ~ROMEDataBase() {};
    virtual bool   Init(const char* name,const char* path,const char* connection) = 0;
-   virtual bool   Read(ROMEStr2DArray *values,const char *path,int runNumber,int eventNumber) = 0;
-   virtual bool   Write(ROMEStr2DArray* values,const char *path,int runNumber,int eventNumber) = 0;
+   virtual bool   Read(ROMEStr2DArray *values,const char *path,int runNumber = 0,int eventNumber = 0) = 0;
+   virtual bool   Write(ROMEStr2DArray* values,const char *path,int runNumber = 0,int eventNumber = 0) = 0;
    virtual char*  GetType() = 0;
    const char*    GetName() { return fName.Data(); };
    virtual char*  GetDescription() = 0;
