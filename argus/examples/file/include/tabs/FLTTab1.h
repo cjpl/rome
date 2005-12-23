0a950347bea766a8b92605c40908da84fa181ba6
schneebeli_m <schneebeli_m@306b0a18-5003-0410-92f3-cc1c673a7936>
2005-12-23 11:14:39 +0000

    added argus examples
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/rome/trunk/rome@807 306b0a18-5003-0410-92f3-cc1c673a7936

---
+++ argus/examples/file/include/tabs/FLTTab1.h
@@ -0,0 +1,68 @@
+////////////////////////////////////////////////////////////////////////////////
+//                                                                            //
+// Editable class file for Tab1 tab.
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef FLTTab1_H
+#define FLTTab1_H
+
+#include <include/generated/FLTTab1_Base.h>
+#include <TGTextEntry.h>
+#include <TGButton.h>
+#include <TGLabel.h>
+#include <TGSplitter.h>
+
+class FLTTab1 : public FLTTab1_Base
+{
+protected:
+   Int_t                fRunNumber;
+   TGTextEntry         *fTextRun;
+   TGTextEntry         *fTextEvent;
+   TGTextEntry         *fTextValue;
+   TGLabel             *fLabelRun;
+   TGLabel             *fLabelEvent;
+   TGLabel             *fLabelValue;
+   TGTextButton        *fBUpdate;
+
+   enum CommandIdentifiers{
+      B_UPDATE
+   };
+
+public:
+   FLTTab1():FLTTab1_Base()
+   {
+      fTextRun = 0;
+      fTextEvent = 0;
+      fTextValue = 0;
+      fBUpdate = 0;
+      fLabelRun = 0;
+      fLabelEvent = 0;
+      fLabelValue = 0;
+   }
+
+   ~FLTTab1()
+   {
+      delete fTextRun;
+      delete fTextEvent;
+      delete fTextValue;
+      delete fBUpdate;
+      delete fLabelRun;
+      delete fLabelEvent;
+      delete fLabelValue;
+   }
+
+   void Init();
+   void MenuClicked(TGPopupMenu *menu,Long_t param);
+   void TabSelected();
+   void TabUnSelected();
+
+   bool ProcessMessage(Long_t msg, Long_t param1, Long_t param2);
+
+private:
+   void Update();
+
+   ClassDef(FLTTab1,1)
+};
+
+#endif   // FLTTab1_H
---
+++ argus/examples/folders/include/tabs/FOTTab.h
@@ -0,0 +1,35 @@
+////////////////////////////////////////////////////////////////////////////////
+//                                                                            //
+// Editable class file for Tab tab.
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef FOTTab_H
+#define FOTTab_H
+
+#include <include/generated/FOTTab_Base.h>
+#include <TClonesArray.h>
+#include "romeproject/include/generated/FORunInfo.h"
+#include "romeproject/include/generated/FOPMTData.h"
+
+class FOTTab : public FOTTab_Base
+{
+protected:
+   FORunInfo*   fRunInfo;
+   TClonesArray* fPMTDatas;
+public:
+   FOTTab():FOTTab_Base()
+   {
+   }
+
+   ~FOTTab()
+   {
+   }
+
+   void Init();
+
+
+   ClassDef(FOTTab,1)
+};
+
+#endif   // FOTTab_H
