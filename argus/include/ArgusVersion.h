7f3a34e682c6b5a2c122fc204eba7589ba7eb635
sawada <sawada@306b0a18-5003-0410-92f3-cc1c673a7936>
2005-12-24 14:53:03 +0000

    removed obsolete files
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/rome/trunk@818 306b0a18-5003-0410-92f3-cc1c673a7936

--- argus/include/ArgusVersion.h
+++
@@ -1,8 +0,0 @@
-#ifndef ARGUSVERSION_H
-#define ARGUSVERSION_H
-
-#define ARGUS_RELEASE "Version 1.06.00"
-#define ARGUS_VERSION_CODE 67072
-#define ARGUS_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
-
-#endif
