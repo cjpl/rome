9ef41bbb6c3e286f4ad081f03c39126362860262
schneebeli_m <schneebeli_m@306b0a18-5003-0410-92f3-cc1c673a7936>
2006-01-06 12:53:48 +0000

    argus - rome interaction improved ROMERint added
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/rome/trunk/rome@842 306b0a18-5003-0410-92f3-cc1c673a7936

---
+++ include/ROMERint.h
@@ -0,0 +1,35 @@
+/********************************************************************
+  ROMERint.h, M. Schneebeli PSI
+
+  $Id: ROMERint.h 480 2005-10-17 12:12:54Z schneebeli_m $
+
+********************************************************************/
+#ifndef ROMERint_H
+#define ROMERint_H
+
+#pragma warning( push )
+#pragma warning( disable : 4800 )
+#include "TRint.h"
+#pragma warning( pop )
+
+class ROMERint : public TRint {
+private:
+   Bool_t fRunning;
+public:
+   ROMERint(const char *appClassName, int *argc, char **argv,
+      void *options = 0, int numOptions = 0, Bool_t noLogo = kFALSE)
+      : TRint(appClassName, argc, argv, options, numOptions,noLogo) 
+   { fRunning = false; };
+   Bool_t HandleTermInput() {
+      if (fRunning)
+         return TRint::HandleTermInput();
+      return true;
+   }
+   void Run(Bool_t retrn) {
+      fRunning = true;
+      TRint::Run(retrn);
+      fRunning = false;
+   }
+};
+
+#endif   // ROMERint_H
