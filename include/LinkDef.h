6c6473fbf154da4673660f96ec52d9f7d976d22c
sawada <sawada@306b0a18-5003-0410-92f3-cc1c673a7936>
2006-02-07 21:03:35 +0000

    possibility of automatic LinkDef generation. bug fix of checking branch folder array.
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/rome/trunk/rome@906 306b0a18-5003-0410-92f3-cc1c673a7936

--- include/LinkDef.h
+++
@@ -1,8 +0,0 @@
-#ifdef __CINT__
-#pragma link C++ class TArrayL64-!;
-#pragma link C++ class ROMEAnalyzer;
-#pragma link C++ class ROMETask;
-#pragma link C++ class ROMETreeInfo;
-#pragma link C++ class TNetFolder;
-#pragma link C++ class ROMENetFolder;
-#endif
