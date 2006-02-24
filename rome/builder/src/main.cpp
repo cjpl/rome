e128a6d1801fd3c6f6f0f6380f3364e22afd3439
sawada <sawada@306b0a18-5003-0410-92f3-cc1c673a7936>
2006-02-24 20:07:01 +0000

    increased version number.
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/rome/trunk@940 306b0a18-5003-0410-92f3-cc1c673a7936

--- rome/builder/src/main.cpp
+++ rome/builder/src/main.cpp
@@ -35,7 +35,7 @@ int main(int argc, char *argv[])
 
    ROMEBuilder* romeb = new ROMEBuilder();
 
-   romeb->romeVersion = "Version 2.3";
+   romeb->romeVersion = "Version 2.4";
    
    ROMEString xmlFile = "";
 
