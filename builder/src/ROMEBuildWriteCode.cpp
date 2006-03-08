5193745128f44865cf90b7f8257bc875ca1775b4
schneebeli_m <schneebeli_m@306b0a18-5003-0410-92f3-cc1c673a7936>
2006-03-08 16:55:30 +0000

    nentry bug removed
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/rome/trunk/rome@976 306b0a18-5003-0410-92f3-cc1c673a7936

--- builder/src/ROMEBuildWriteCode.cpp
+++ builder/src/ROMEBuildWriteCode.cpp
@@ -1888,6 +1888,7 @@ Bool_t ROMEBuilder::WriteTaskH()
       }
       if (array) {
          buffer.AppendFormatted("   int j;\n");
+         buffer.AppendFormatted("   int nentry;\n");
       }
       for (i=0;i<numOfHistos[iTask];i++) {
          if (histoArraySize[iTask][i]=="1") {
@@ -7689,6 +7690,7 @@ Bool_t ROMEBuilder::WriteEventLoopCpp()
       for (j=0;j<numOfHistos[taskHierarchyClassIndex[i]] && !breaking;j++) {
          if (histoArraySize[taskHierarchyClassIndex[i]][j]!="1") {
             buffer.AppendFormatted("   int i;\n");
+            buffer.AppendFormatted("   int nentry;\n");
             breaking = true;
          }
       }
