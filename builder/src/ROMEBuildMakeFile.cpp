d03cde47c6f3bb28dbb254055df93368701208d5
sawada <sawada@306b0a18-5003-0410-92f3-cc1c673a7936>
2006-06-14 15:52:07 +0000

    bugfix. Some variable names in Makefile should be capitalized.
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/rome/trunk/rome@1151 306b0a18-5003-0410-92f3-cc1c673a7936

--- builder/src/ROMEBuildMakeFile.cpp
+++ builder/src/ROMEBuildMakeFile.cpp
@@ -775,16 +775,16 @@ void ROMEBuilder::WriteMakefileLibsAndFlags(ROMEString& buffer)
 #if defined( R__UNIX )
    // equal signs below should be '=' to allow change in Makefile.usr
    buffer.AppendFormatted("ifndef %sCFLAGS\n",shortCut.ToUpper(tmp));
-   buffer.AppendFormatted("%sCFLAGS = $(%suserflags) $(%sOPT)\n",shortCut.ToUpper(tmp),shortCut.ToLower(tmp),shortCut.ToLower(tmp));
+   buffer.AppendFormatted("%sCFLAGS = $(%suserflags) $(%sOPT)\n",shortCut.ToUpper(tmp),shortCut.ToLower(tmp),shortCut.ToUpper(tmp));
    buffer.AppendFormatted("endif\n\n");
    buffer.AppendFormatted("ifndef %sCXXFLAGS\n",shortCut.ToUpper(tmp));
-   buffer.AppendFormatted("%sCXXFLAGS = $(%suserflags) $(%sOPT)\n",shortCut.ToUpper(tmp),shortCut.ToLower(tmp),shortCut.ToLower(tmp));
+   buffer.AppendFormatted("%sCXXFLAGS = $(%suserflags) $(%sOPT)\n",shortCut.ToUpper(tmp),shortCut.ToLower(tmp),shortCut.ToUpper(tmp));
    buffer.AppendFormatted("endif\n\n");
    buffer.AppendFormatted("ifndef %sFFLAGS\n",shortCut.ToUpper(tmp));
-   buffer.AppendFormatted("%sFFLAGS = $(%suserflags) $(%sOPT)\n",shortCut.ToUpper(tmp),shortCut.ToLower(tmp),shortCut.ToLower(tmp));
+   buffer.AppendFormatted("%sFFLAGS = $(%suserflags) $(%sOPT)\n",shortCut.ToUpper(tmp),shortCut.ToLower(tmp),shortCut.ToUpper(tmp));
    buffer.AppendFormatted("endif\n\n");
    buffer.AppendFormatted("ifndef %sLDFLAGS\n",shortCut.ToUpper(tmp));
-   buffer.AppendFormatted("%sLDFLAGS = $(%suserflags) $(%sOPT)\n",shortCut.ToUpper(tmp),shortCut.ToLower(tmp),shortCut.ToLower(tmp));
+   buffer.AppendFormatted("%sLDFLAGS = $(%suserflags) $(%sOPT)\n",shortCut.ToUpper(tmp),shortCut.ToLower(tmp),shortCut.ToUpper(tmp));
    buffer.AppendFormatted("endif\n\n");
    buffer.AppendFormatted("CXX = g++\n");
 
