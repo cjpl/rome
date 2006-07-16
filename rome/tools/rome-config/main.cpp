851291648f5789427acd3eef9adb024607e4eef8
sawada <sawada@306b0a18-5003-0410-92f3-cc1c673a7936>
2006-07-16 03:52:11 +0000

    added revision-greater/less/equal.
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/rome/trunk@1277 306b0a18-5003-0410-92f3-cc1c673a7936

--- rome/tools/rome-config/main.cpp
+++ rome/tools/rome-config/main.cpp
@@ -5,20 +5,25 @@
   $Id$
 
 ********************************************************************/
+#include <stdlib.h>
 #include <Riostream.h>
 #include "ROMEVersion.h"
 
 void help()
 {
    cout<<" Usage: rome-config [options]"<<endl;
-   cout<<"  --version             Print the ROME version"<<endl;
-   cout<<"  --revision            Print the ROME revision"<<endl;
-   cout<<"  --release             Print if release version [yes/no]"<<endl;
-   cout<<"  --help                Print this message"<<endl;
+   cout<<"  --version                  Print the ROME version"<<endl;
+   cout<<"  --revision                 Print the ROME revision"<<endl;
+   cout<<"  --revision-greater number  Print if revision is greater than number [yes/no]"<<endl;
+   cout<<"  --revision-less number     Print if revision is less than number [yes/no]"<<endl;
+   cout<<"  --revision-equal number    Print if revision is equal to number [yes/no]"<<endl;
+   cout<<"  --release                  Print if release version [yes/no]"<<endl;
+   cout<<"  --help                     Print this message"<<endl;
 }
 
 int main(int argc, char *argv[])
 {
+   char* cstop = 0;
    if (argc == 1) {
       help();
       return 0;
@@ -31,6 +36,42 @@ int main(int argc, char *argv[])
 
    if (!strcmp(argv[1], "--revision")) {
       cout<<ROME_REVISION_CODE<<endl;
+      return 0;
+   }
+
+   if (!strcmp(argv[1], "--revision-greater")) {
+      if (argc < 3 ){
+         help();
+         return 0;
+      }
+      if (ROME_REVISION_CODE > strtol(argv[2], &cstop, 10))
+         cout<<"yes"<<endl;
+      else
+         cout<<"no"<<endl;
+      return 0;
+   }
+
+   if (!strcmp(argv[1], "--revision-less")) {
+      if (argc < 3 ){
+         help();
+         return 0;
+      }
+      if (ROME_REVISION_CODE < strtol(argv[2], &cstop, 10))
+         cout<<"yes"<<endl;
+      else
+         cout<<"no"<<endl;
+      return 0;
+   }
+
+   if (!strcmp(argv[1], "--revision-equal")) {
+      if (argc < 3 ){
+         help();
+         return 0;
+      }
+      if (ROME_REVISION_CODE == strtol(argv[2], &cstop, 10))
+         cout<<"yes"<<endl;
+      else
+         cout<<"no"<<endl;
       return 0;
    }
 
