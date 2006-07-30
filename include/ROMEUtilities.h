c6ebd928562c8433eee55cbca67dac99bd4302ab
sawada <sawada@306b0a18-5003-0410-92f3-cc1c673a7936>
2006-07-30 03:30:49 +0000

    speed up of byte swapping * use assembler byte swap under Mac OSX.
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/rome/trunk/rome@1340 306b0a18-5003-0410-92f3-cc1c673a7936

--- include/ROMEUtilities.h
+++ include/ROMEUtilities.h
@@ -7,7 +7,11 @@
 #ifndef ROMEUtilities_H
 #define ROMEUtilities_H
 
+#include <RConfig.h>
 #include <Bytes.h>
+#if defined( R__MACOSX )
+#   include <libkern/OSByteOrder.h>
+#endif
 
 class ROMEString;
 class ROMEStrArray;
@@ -28,85 +32,82 @@ namespace ROMEUtilities {
 //
 // Byte swapping big endian <-> little endian
 //
-
-inline void ROMEUtilities::ByteSwap( UShort_t *aValue )
+inline void ROMEUtilities::ByteSwap( UShort_t *x )
 {
-#if defined(R__USEASMSWAP)
-   UShort_t tmp = Rbswap_16(*aValue);
-   memcpy(aValue, &tmp, sizeof(UShort_t));
+#if defined( R__USEASMSWAP )
+   *x = Rbswap_16(*x);
+#elif defined( R__MACOSX )
+   *x = OSSwapInt16(*x);
 #else
-   Byte_t _tmp;
-   _tmp = *( (Byte_t *)( aValue ) );
-   *( (Byte_t *)( aValue ) ) = *( ( (Byte_t *)( aValue ) ) + 1 );
-   *( ( (Byte_t *)( aValue ) ) + 1 ) = _tmp;
+   *x = ((UShort_t)((((UShort_t)(*x) & 0xff00) >> 8) |
+                    (((UShort_t)(*x) & 0x00ff) << 8)));
 #endif
 }
 
-inline void ROMEUtilities::ByteSwap( Short_t *aValue )
+inline void ROMEUtilities::ByteSwap( Short_t *x )
 {
-    ByteSwap( (UShort_t *)aValue );
+    ByteSwap( (UShort_t *)x );
 }
 
-inline void ROMEUtilities::ByteSwap( UInt_t *aValue )
+inline void ROMEUtilities::ByteSwap( UInt_t *x )
 {
-#if defined(R__USEASMSWAP)
-   UInt_t tmp = Rbswap_32(*aValue);
-   memcpy(aValue, &tmp, sizeof(UInt_t));
+#if defined( R__USEASMSWAP )
+   *x = Rbswap_32(*x);
+#elif defined( R__MACOSX ) 
+   *x = OSSwapInt32(*x);
 #else
-   Byte_t _tmp;
-   _tmp = *( (Byte_t *)( aValue ) );
-   *( (Byte_t *)( aValue ) ) = *( ( (Byte_t *)( aValue ) ) + 3 );
-   *( ((Byte_t *)( aValue ) ) + 3) = _tmp;
-
-   _tmp = *( ( (Byte_t *)( aValue ) ) + 1);
-   *( ( (Byte_t *)( aValue ) ) + 1 ) = *( ( (Byte_t *)( aValue ) ) + 2 );
-   *( ( (Byte_t *)( aValue ) ) + 2 ) = _tmp;
+   *x =  ((UInt_t)((((UInt_t)(*x) & 0xff000000) >> 24) |
+                   (((UInt_t)(*x) & 0x00ff0000) >>  8) |
+                   (((UInt_t)(*x) & 0x0000ff00) <<  8) |
+                   (((UInt_t)(*x) & 0x000000ff) << 24)));
 #endif
 }
 
-inline void ROMEUtilities::ByteSwap( Int_t *aValue )
+inline void ROMEUtilities::ByteSwap( Int_t *x )
 {
-    ByteSwap( (UInt_t *)aValue );
+    ByteSwap( (UInt_t *)x );
 }
 
-inline void ROMEUtilities::ByteSwap( Float_t *aValue )
+inline void ROMEUtilities::ByteSwap( Float_t *x )
 {
-    ByteSwap( (UInt_t *)aValue );
+    ByteSwap( (UInt_t *)x );
 }
 
-inline void ROMEUtilities::ByteSwap( ULong64_t *aValue )
+inline void ROMEUtilities::ByteSwap( ULong64_t *x )
 {
-#if defined(R__USEASMSWAP)
-   ULong64_t tmp = Rbswap_64(*aValue);
-   memcpy(aValue, &tmp, sizeof(ULong64_t));
+#if defined( R__USEASMSWAP )
+   *x = Rbswap_64(*x);
+#elif defined( R__MACOSX )
+   *x = OSSwapInt64(*x);
+#elif defined( R__VISUAL_CPLUSPLUS )
+   *x = ((ULong64_t)((((ULong64_t)(*x) & 0xff00000000000000UI64) >> 56) |
+                     (((ULong64_t)(*x) & 0x00ff000000000000UI64) >> 40) |
+                     (((ULong64_t)(*x) & 0x0000ff0000000000UI64) >> 24) |
+                     (((ULong64_t)(*x) & 0x000000ff00000000UI64) >>  8) |
+                     (((ULong64_t)(*x) & 0x00000000ff000000UI64) <<  8) |
+                     (((ULong64_t)(*x) & 0x0000000000ff0000UI64) << 24) |
+                     (((ULong64_t)(*x) & 0x000000000000ff00UI64) << 40) |
+                     (((ULong64_t)(*x) & 0x00000000000000ffUI64) << 56)));
 #else
-   Byte_t _tmp;
-   _tmp= *( (Byte_t *)( aValue ) );
-   *( (Byte_t *)( aValue ) ) = *(((Byte_t *)( aValue ) ) + 7 );
-   *( ( (Byte_t *)( aValue ) ) + 7 ) = _tmp;
-
-   _tmp = *( ( (Byte_t *)( aValue ) ) + 1 );
-   *( ( (Byte_t *)( aValue ) ) + 1 ) = *( ( (Byte_t *)( aValue ) ) + 6 );
-   *( ( (Byte_t *)( aValue ) ) + 6 ) = _tmp;
-
-   _tmp = *(((Byte_t *)( aValue ))+2);
-   *( ( (Byte_t *)( aValue ) ) + 2 ) = *(((Byte_t *)( aValue ) ) + 5 );
-   *( ( (Byte_t *)( aValue ) ) + 5 ) = _tmp;
-
-   _tmp = *( ( (Byte_t *)( aValue ) ) + 3 );
-   *( ( (Byte_t *)( aValue ) ) + 3 ) = *(((Byte_t *)( aValue ) ) + 4 );
-   *( ( (Byte_t *)( aValue ) ) + 4 ) = _tmp;
+   *x = ((ULong64_t)((((ULong64_t)(*x) & 0xff00000000000000ULL) >> 56) |
+                     (((ULong64_t)(*x) & 0x00ff000000000000ULL) >> 40) |
+                     (((ULong64_t)(*x) & 0x0000ff0000000000ULL) >> 24) |
+                     (((ULong64_t)(*x) & 0x000000ff00000000ULL) >>  8) |
+                     (((ULong64_t)(*x) & 0x00000000ff000000ULL) <<  8) |
+                     (((ULong64_t)(*x) & 0x0000000000ff0000ULL) << 24) |
+                     (((ULong64_t)(*x) & 0x000000000000ff00ULL) << 40) |
+                     (((ULong64_t)(*x) & 0x00000000000000ffULL) << 56)));
 #endif
 }
 
-inline void ROMEUtilities::ByteSwap( Long64_t *aValue )
+inline void ROMEUtilities::ByteSwap( Long64_t *x )
 {
-    ByteSwap( (ULong64_t *)aValue );
+    ByteSwap( (ULong64_t *)x );
 }
 
-inline void ROMEUtilities::ByteSwap( Double_t *aValue )
+inline void ROMEUtilities::ByteSwap( Double_t *x )
 {
-    ByteSwap( (ULong64_t *)aValue );
+    ByteSwap( (ULong64_t *)x );
 }
 
 
