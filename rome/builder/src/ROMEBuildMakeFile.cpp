66bc3661291fc797ea4ed27bbff07f57b545e339
sawada <sawada@306b0a18-5003-0410-92f3-cc1c673a7936>
2007-03-19 18:28:30 +0000

    small improvement of target count in Makefile.
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/rome/trunk@1755 306b0a18-5003-0410-92f3-cc1c673a7936

--- rome/builder/src/ROMEBuildMakeFile.cpp
+++ rome/builder/src/ROMEBuildMakeFile.cpp
@@ -998,8 +998,10 @@ void ROMEBuilder::WriteMakefileLibsAndFlags(ROMEString& buffer)
    buffer.AppendFormatted("      NTARGETS_TOTAL := $(shell $(MAKE) NTARGETS_STOP=yes -n $(MAKECMDGOALS) | \\\n");
    buffer.AppendFormatted("                          grep NTARGETS_MAGIC | wc -l)\n");
    buffer.AppendFormatted("      CREATE_TARGETS_FILE := $(shell $(MAKE) NTARGETS_STOP=yes -n $(MAKECMDGOALS) | \\\n");
-   buffer.AppendFormatted("                               grep NTARGETS_MAGIC | awk '{print $(NTARGETS_TOTAL)-NR+1 \" \" $$0}' > $(NTARGETS_FILE))\n");
-   buffer.AppendFormatted("      NTARGETS = $(shell grep $(subst .,\\.,$1) $(NTARGETS_FILE) | cut -d ' ' -f 1)\n");
+   buffer.AppendFormatted("                               grep NTARGETS_MAGIC | \\\n");
+   buffer.AppendFormatted("                               awk '{print $(NTARGETS_TOTAL)-NR+1 \" \" $$0}' > \\\n");
+   buffer.AppendFormatted("                               $(NTARGETS_FILE))\n");
+   buffer.AppendFormatted("      NTARGETS = $(shell grep -m 1 $(subst .,\\.,$1) $(NTARGETS_FILE) | cut -d ' ' -f 1)\n");
    buffer.AppendFormatted("   endif\n");
    buffer.AppendFormatted("   Q = @\n");
    buffer.AppendFormatted("   define %sechoing\n",shortCut.ToLower(tmp));
