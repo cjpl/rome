65498e4168d285d9ecd238cce1177c63e2ae5493
sawada <sawada@306b0a18-5003-0410-92f3-cc1c673a7936>
2007-05-11 13:16:22 +0000

    added verious type of class branches in example.  - class pointer with splitting  - class pointer without splitting  - class object with splitting  - class object without splitting All types works without problem on linux.
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/rome/trunk/rome@1879 306b0a18-5003-0410-92f3-cc1c673a7936

--- examples/tree_write_read2/RawData.cpp
+++ examples/tree_write_read2/RawData.cpp
@@ -2,3 +2,60 @@
 
 ClassImp(RawData)
 
+//______________________________________________________________________________
+RawData::RawData()
+:TObject()
+,fDataLength(0)
+,fData(0)
+{}
+
+//______________________________________________________________________________
+RawData::RawData(const RawData &c)
+:TObject(c)
+,fDataLength(c.fDataLength)
+,fData(0)
+{
+   if (fDataLength) {
+      fData = new char[fDataLength];
+   }
+   memcpy(fData, c.fData, fDataLength);
+}
+
+//______________________________________________________________________________
+RawData &RawData::operator=(const RawData &c)
+{
+   if (this != &c) {
+      if (fDataLength != c.fDataLength) {
+         fDataLength = c.fDataLength;
+         delete [] fData;
+         if (fDataLength) {
+            fData = new char[fDataLength];
+         } else {
+            fData = 0;
+         }
+      }
+      if (fDataLength) {
+         memcpy(fData, c.fData, fDataLength);
+      }
+   }
+   return *this;
+}
+
+//______________________________________________________________________________
+RawData::~RawData()
+{
+   if(fData) {delete [] fData; fData = 0;}
+}
+
+//______________________________________________________________________________
+void RawData::SetDataSize(Int_t size)
+{
+   if (fData) {
+      delete [] fData;
+   }
+   if (size) {
+      fData = new char[size];
+   } else {
+      fData = 0;
+   }
+}
