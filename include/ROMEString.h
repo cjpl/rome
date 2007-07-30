fefa103742c3bfe5482ade088ce64e9efdedc592
sawada <sawada@306b0a18-5003-0410-92f3-cc1c673a7936>
2007-07-30 00:54:07 +0000

    fixed error at the previous revision.
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/rome/trunk/rome@2010 306b0a18-5003-0410-92f3-cc1c673a7936

--- include/ROMEString.h
+++ include/ROMEString.h
@@ -26,8 +26,8 @@ public:
    ROMEString&  AppendFormatted(const char* format=NULL,...) G_GNUC_PRINTF(2, 3);
    ROMEString&  InsertFormatted(Ssiz_t position,const char* format=NULL,...) G_GNUC_PRINTF(3, 4);
    ROMEString&  SetFormatted(const char* format=NULL,...) G_GNUC_PRINTF(2, 3);
-   static char* Format(const char *format, va_list ap) G_GNUC_PRINTF(2, 0);;
-   static char* SlowFormat(const char *format, va_list ap, int hint) G_GNUC_PRINTF(2, 0);;
+   static char* Format(const char *format, va_list ap) G_GNUC_PRINTF(1, 0);
+   static char* SlowFormat(const char *format, va_list ap, int hint) G_GNUC_PRINTF(1, 0);
 #endif
    Int_t        NumberOfOccurrence(ROMEString& subString);
    Int_t        NumberOfOccurrence(const char* subString);
