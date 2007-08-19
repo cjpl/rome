c6b254e9309f91f75d23c1d4edbe08565bfa6c7e
sawada <sawada@306b0a18-5003-0410-92f3-cc1c673a7936>
2007-08-19 20:17:16 +0000

    show warning when connected task is not found when generating DOT file.
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/rome/trunk/rome@2073 306b0a18-5003-0410-92f3-cc1c673a7936

--- builder/src/ROMEBuildWriteCode.cpp
+++ builder/src/ROMEBuildWriteCode.cpp
@@ -13840,7 +13840,7 @@ Bool_t ROMEBuilder::WriteDOT()
             }
          }
          if (!found) {
-//            cerr<<"Warning : task '"<<taskHierarchyConnectedFrom[i][j]<<"' connected from '"<<name<<"' was not found."<<endl;
+            cerr<<"Warning : task '"<<taskHierarchyConnectedFrom[i][j]<<"' connected from '"<<name<<"' was not found."<<endl;
          } else {
             buffer.AppendFormatted("%*s%s -> %s;\n", 3 * depth, "", taskHierarchyConnectedFrom[i][j].Data(), name.Data());
          }
