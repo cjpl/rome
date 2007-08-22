b697968fee07c9c634b7be5b8ced590761ad9f7f
sawada <sawada@306b0a18-5003-0410-92f3-cc1c673a7936>
2007-08-22 14:42:16 +0000

    fixed typo (5.5 and 5,5)
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/rome/trunk/rome@2084 306b0a18-5003-0410-92f3-cc1c673a7936

--- builder/src/ROMEBuildWriteCode.cpp
+++ builder/src/ROMEBuildWriteCode.cpp
@@ -12627,7 +12627,7 @@ void ROMEBuilder::WriteHTMLDoku()
                                  shortCut.Data(),mainProgName.Data(),
                                  mapFileName.Data());
          gSystem->Exec(dotCommand.Data());
-         dotCommand.SetFormatted("%s -Tgif -Gsize=\"5.5\" %s src/generated/%s%s.dot -o %s",dotexe,dotOption.Data(),
+         dotCommand.SetFormatted("%s -Tgif -Gsize=\"5,5\" %s src/generated/%s%s.dot -o %s",dotexe,dotOption.Data(),
                                  shortCut.Data(),mainProgName.Data(),
                                  mapIconName.Data());
          gSystem->Exec(dotCommand.Data());
