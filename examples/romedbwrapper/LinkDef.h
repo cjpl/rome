54f93f757600dc14e43298a764d3436772deb3ca
sawada <sawada@306b0a18-5003-0410-92f3-cc1c673a7936>
2007-09-01 15:18:35 +0000

    ROOT v5.17.02 support. removed userMakefile example.
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/rome/trunk/rome@2099 306b0a18-5003-0410-92f3-cc1c673a7936

---
+++ examples/romedbwrapper/LinkDef.h
@@ -0,0 +1,16 @@
+//  $Id$
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class ROMEString+;
+#pragma link C++ class ROMEStrArray+;
+#pragma link C++ class ROMEStr2DArray+;
+#pragma link C++ class ROMEPath;
+#pragma link C++ class ROMESQL;
+#pragma link C++ class ROMESQLite3;
+#pragma link C++ class ROMESQLDataBase;
+#pragma link C++ class ROMEDataBase;
+#endif
