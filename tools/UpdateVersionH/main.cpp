34fb01974561ac6f988accb6d7bc68dc9493f737
sawada <sawada@306b0a18-5003-0410-92f3-cc1c673a7936>
2007-10-04 12:41:55 +0000

    modified release identifying variable.
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/rome/tags/rome_v2.9@2129 306b0a18-5003-0410-92f3-cc1c673a7936

--- tools/UpdateVersionH/main.cpp
+++ tools/UpdateVersionH/main.cpp
@@ -23,7 +23,7 @@
 // change following values when release
 const Int_t romeMajor = 2;
 const Int_t romeMinor = 9;
-const Int_t isStableVersion = 0; // 0: development version, 1: stable version
+const Int_t isStableVersion = 1; // 0: development version, 1: stable version
 //------
 
 /*
