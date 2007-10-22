1b1c1019d30073e4b9300d9f21085df3151061eb
sawada <sawada@306b0a18-5003-0410-92f3-cc1c673a7936>
2007-10-22 18:13:29 +0000

    deactivate showing warning message to disable graphics when no graphics or back mode is specified by command line option.
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/rome/trunk/rome@2183 306b0a18-5003-0410-92f3-cc1c673a7936

--- builder/src/ROMEBuildWriteCode.cpp
+++ builder/src/ROMEBuildWriteCode.cpp
@@ -12463,7 +12463,7 @@ Bool_t ROMEBuilder::WriteMain()
    buffer.AppendFormatted("         nographics = true;\n");
    buffer.AppendFormatted("      }\n");
 #if defined( R__UNIX )
-   buffer.AppendFormatted("      if (!gSystem->Getenv(\"DISPLAY\")) {\n");
+   buffer.AppendFormatted("      if (!nographics && !batch && !daemon && !gSystem->Getenv(\"DISPLAY\")) {\n");
    buffer.AppendFormatted("         cerr<<\"Graphics is disabled because DISPLAY is not set.\"<<endl;\n");
    buffer.AppendFormatted("         nographics = true;\n");
    buffer.AppendFormatted("      }\n");
