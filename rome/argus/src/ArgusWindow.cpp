029b397c1ce99fecf1896c8ac9439f4c0ec028f7
yamada <yamada@306b0a18-5003-0410-92f3-cc1c673a7936>
2007-11-01 18:45:37 +0000

    A workaround for BadWindow/BadDrawable error on X11.
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/rome/trunk@2240 306b0a18-5003-0410-92f3-cc1c673a7936

--- rome/argus/src/ArgusWindow.cpp
+++ rome/argus/src/ArgusWindow.cpp
@@ -705,7 +705,18 @@ Bool_t ArgusWindow::ProcessMessage(Long_t msg, Long_t param1, Long_t /*param2*/)
                gROME->GetWindow()->SetSubWindowTimeStringAt(fWindowId,GetTimeStatisticsString(str));
                ClearEventHandlingRequest();
                ClearEventHandlingForced();
-               TGMainFrame::CloseWindow();
+
+               // When a sub window is newly opened or it is closed,
+               // calling TGMainFrame::CloseWindow() causes BadWindow
+               // and/or BadDrawable error on X11, which terminates
+               // the program. In order avoid this, just unmap the sub
+               // window rather than closing it. This also makes it
+               // possible to map the sub window again instead of
+               // creating a new sub window.
+
+//               TGMainFrame::CloseWindow();
+               UnmapWindow();
+
                tab->SetRegisteringActive(true);
                if (!tab->IsTabActive()) {
                   tab->UnRegisterObjects();
