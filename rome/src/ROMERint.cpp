8aedeaeb54c4c629358c7b3afc85a0a82e622dbd
sawada <sawada@306b0a18-5003-0410-92f3-cc1c673a7936>
2008-02-01 18:15:57 +0000

    modified event number output in signal handler.
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/rome/trunk@2373 306b0a18-5003-0410-92f3-cc1c673a7936

--- rome/src/ROMERint.cpp
+++ rome/src/ROMERint.cpp
@@ -130,14 +130,32 @@ void UnixSignal(ESignals sig, SigHandler_t handler)
 static void SigHandler(ESignals sig)
 {
    // Unix signal handler.
+   Long64_t event;
    if (gROME) {
+      event = gROME->GetCurrentEventNumber();
       cerr<<"Signal";
       if (static_cast<Int_t>(sig) >= 0 &&
           static_cast<Int_t>(sig) < static_cast<Int_t>(kMAXSIGNALS)) {
          cerr<<"("<<gSignalMap[static_cast<Int_t>(sig)].fSigName<<")";
       }
-      cerr<<" caught at run="<<gROME->GetCurrentRunNumber()<<
-            ", event="<<gROME->GetCurrentEventNumber()<<endl;
+      cerr<<" caught at run="<<gROME->GetCurrentRunNumber();
+      switch (event) {
+      case kEventNumberInit :
+         cerr<<", Init"<<endl;
+         break;
+      case kEventNumberBeginOfRun:
+         cerr<<", BeginOfRun"<<endl;
+         break;
+      case kEventNumberEndOfRun:
+         cerr<<", EndOfRun"<<endl;
+         break;
+      case kEventNumberTerminate:
+         cerr<<", Terminate"<<endl;
+         break;
+      default:
+         cerr<<", event="<<event<<endl;
+         break;
+      }
    }
    cleaning();
    if (gSystem)
