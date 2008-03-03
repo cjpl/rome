0c5b803a4e697d6e6d1a50a0f945ea48ac1e6491
sawada <sawada@306b0a18-5003-0410-92f3-cc1c673a7936>
2008-03-03 16:51:55 +0000

    partial compatibility fix with ROOT 4.2.0.
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/rome/trunk@2383 306b0a18-5003-0410-92f3-cc1c673a7936

--- rome/src/ROMETree.cpp
+++ rome/src/ROMETree.cpp
@@ -100,7 +100,11 @@ Long64_t ROMETree::AutoSave(Option_t *option)
    Long64_t ret = 0;
    if (fAutoSaveSize > 0 && isWrite() && fTree && fTree->GetCurrentFile()) {
       ret = fTree->AutoSave(option);
+#if (ROOT_VERSION_CODE > ROOT_VERSION(4,4,2))
       fLastSaveSize = fTree->GetCurrentFile()->GetBytesWritten();
+#else
+      fLastSaveSize = static_cast<Long64_t>(fTree->GetCurrentFile()->GetBytesWritten());
+#endif
    }
    return ret;
 }
@@ -110,7 +114,11 @@ Bool_t ROMETree::CheckAutoSave()
 {
    Long64_t currentSize;
    if (fAutoSaveSize > 0 && isWrite() && fTree && fTree->GetCurrentFile()) {
+#if (ROOT_VERSION_CODE > ROOT_VERSION(4,4,2))
       currentSize = fTree->GetCurrentFile()->GetBytesWritten();
+#else
+      currentSize = static_cast<Long64_t>(fTree->GetCurrentFile()->GetBytesWritten());
+#endif
       if (currentSize < fLastSaveSize) {
          // probably new file is created.
          fLastSaveSize = 0;
