18ba204860fe91041688b6d9803aa0c7a61814ec
sawada <sawada@306b0a18-5003-0410-92f3-cc1c673a7936>
2008-03-03 16:51:55 +0000

    partial compatibility fix with ROOT 4.2.0.
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/rome/trunk/rome@2383 306b0a18-5003-0410-92f3-cc1c673a7936

--- src/ROMETask.cpp
+++ src/ROMETask.cpp
@@ -28,10 +28,12 @@
 #include <ROMERint.h>
 #include "ROMEiostream.h"
 
-#include "TH1.h"
-#include "TH1K.h"
-#include "TH2.h"
-#include "TH3.h"
+#include <TH1.h>
+#include <TH1K.h>
+#include <TH2.h>
+#include <TH3.h>
+#include <TProfile.h>
+#include <TProfile2D.h>
 
 #include "TCutG.h"
 #include "ROMETCutG.h"
