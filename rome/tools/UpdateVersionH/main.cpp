a75e55cc7cb7977f0fc36c8ba5396f3a62d7c85b
sawada <sawada@306b0a18-5003-0410-92f3-cc1c673a7936>
2010-12-22 10:51:09 +0000

    updated minor version.
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/rome/trunk@2567 306b0a18-5003-0410-92f3-cc1c673a7936

--- rome/tools/UpdateVersionH/main.cpp
+++ rome/tools/UpdateVersionH/main.cpp
@@ -24,7 +24,7 @@
 //------
 // change following values when release
 const Int_t romeMajor       =  2;
-const Int_t romeMinor       = 12;
+const Int_t romeMinor       = 13;
 const Int_t isStableVersion =  0; // 0: development version, 1: stable version
 //------
 
